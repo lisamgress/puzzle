a7ba8a16da97f7c412edc4487751d2b0abc46c40
Lisa Gress <lisamgress@gmail.com>
2014-03-14 15:34:20 -0700

    Add a check to see which tiles are eligible to move (tiles next to the empty tile).

--- js/main.js
+++ js/main.js
@@ -19,7 +19,43 @@ function createPuzzleBoard() {
 	}
 }
 
+function findMoveableTiles(emptyRow, emptyCol) {
+	// clear any existing moveable tags
+	$(".tile").removeClass("moveable");
+
+	// check each tile to see if it is moveable and if it is, tag it.
+	$(".tile").each(function() {
+		var tile = $(this).attr("class").split(" ");
+		var tileRow = parseInt(tile[1][3], 10);
+		var tileCol = parseInt(tile[2][3], 10);
+
+		var emptyTileRow = parseInt(emptyRow[3], 10);
+		var emptyTileCol = parseInt(emptyCol[3], 10);
+
+		if((Math.abs(tileRow - emptyTileRow) == 1 && Math.abs(tileCol - emptyTileCol) == 0 )||
+			(Math.abs(tileRow - emptyTileRow) == 0 && Math.abs(tileCol - emptyTileCol) == 1)) {
+			$(this).addClass("moveable");
+		}
+	})
+}
+
+
 $(document).ready(function() {
 	createPuzzleBoard();
+
+	// start game with an empty tile in position (4, 4)
+	var emptyTileRowClass = "row4";
+	var emptyTileColClass = "col4";
+
+	findMoveableTiles(emptyTileRowClass, emptyTileColClass);
+
+	// when a tile is clicked, move it to the location of the empty tile
+	$(".tile").click(function() {
+		var tileCurrentLocation = $(this).attr("class").split(" ");
+		$(this).attr("class", "tile " + emptyTileRowClass + " " + emptyTileColClass);
+		emptyTileRowClass = tileCurrentLocation[1];
+		emptyTileColClass = tileCurrentLocation[2];
+		findMoveableTiles(emptyTileRowClass, emptyTileColClass);
+	})
 })
 
