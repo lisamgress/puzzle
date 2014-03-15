3aee2ff4bb6192277cbcdd4d5aee76e14c30a935
Lisa Gress <lisamgress@gmail.com>
2014-03-14 20:11:28 -0700

    Implement method to check for a complete winning puzzle

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function createPuzzleBoard() {
 
 	for(i = 1; i <= 15; i++) {
 		var newTile = $("<div>" + i + "</div>");
+		newTile.attr("id", i);
 		newTile.addClass("tile");
 
 		if (col > 4) {
@@ -70,21 +71,59 @@ function shufflePuzzleboard(emptyTile) {
 	return emptyTile;
 }
 
+function checkForWin() {
+	var winningBoard = {
+		1 : ["row1", "col1"],
+		2 : ["row1", "col2"],
+		3 : ["row1", "col3"],
+		4 : ["row1", "col4"],
+		5 : ["row2", "col1"],
+		6 : ["row2", "col2"],
+		7 : ["row2", "col3"],
+		8 : ["row2", "col4"],
+		9 : ["row3", "col1"],
+		10 : ["row3", "col2"],
+		11 : ["row3", "col3"],
+		12 : ["row3", "col4"],
+		13 : ["row4", "col1"],
+		14 : ["row4", "col2"],
+		15 : ["row4", "col3"],
+	}
+
+	var tiles = $(".tile");
+
+	for(var i = 0; i < tiles.length; i++) {
+		var tile = $(tiles[i]);
+		var id = tile.attr("id");
+
+		if(!($(tile).hasClass(winningBoard[id][0]) && $(tile).hasClass(winningBoard[id][1]))) {
+			return false;
+		} 
+	}
+	return true;
+}
+
 $(document).ready(function() {
 	// start game with an empty tile in position (4, 4)
 	var emptyTile = ["row4", "col4"];
-	
+
 	createPuzzleBoard();
 	findMoveableTiles(emptyTile);
 
 	// shuffle puzzle board
 	$("#shufflebutton").click(function() {
 		emptyTile = shufflePuzzleboard(emptyTile);
+		$(this).addClass("shuffled");
 	})
 
 	// when a tile is clicked, move it to the location of the empty tile
 	$(".tile").click(function() {
 		emptyTile = swapTiles($(this), emptyTile);
+		var win = checkForWin();
+
+		if($("#shufflebutton").hasClass("shuffled") && win) {
+			$("#messageboard").append("<p>YOU WIN!!</p>");
+		}
 	})
 })
 
